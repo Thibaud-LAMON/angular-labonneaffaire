4ea97c1878c8c197f30524f714e7520fe7c61a55
Thibaud-LAMON <thib.lamon@gmail.com>
2023-12-18 14:45:58 +0100

    Utiliser les méthodes Property binding & Event binding

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -8,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class ArticleComponent implements OnInit {
   titreArticle: string = "Titre de l'article";
   prixArticle: number = 12;
+  textAltImg: string = "Titre alternatif de l'image";
+  urlImg: string = 'https://via.placeholder.com/400x250';
+  totalNbrLike: number = 0;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  //nomenclature méthodes events : onNomDeMethode
+  onLike() {
+    //this permet de manipuler des variables dans une classe
+    this.totalNbrLike++;
+  }
 }
