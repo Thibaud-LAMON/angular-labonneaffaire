aa94def9a22acddf913cc0a5b3e0f902942cb718
Thibaud-LAMON <thib.lamon@gmail.com>
2023-12-18 16:53:33 +0100

    Créer des boucles avec la directive ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,31 @@ export class AppComponent {
   onAffiche(arg: string) {
     return (this.message = "Merci d'avoir voté pour l'article : " + arg);
   }
+
+  liste = [
+    {
+      titreArticle: 'Vélo',
+      prixArticle: 80,
+      description: 'Un super vélo tout terrain',
+      urlImg: '../assets/images/velo.jpeg',
+      textAltImg: 'Un vélo',
+      available: false,
+    },
+    {
+      titreArticle: 'TV',
+      prixArticle: 230,
+      description: "Très bonne qualité d'écran",
+      urlImg: '../assets/images/tv.jpeg',
+      textAltImg: 'Une télévision',
+      available: true,
+    },
+    {
+      titreArticle: 'Jouet',
+      prixArticle: 14,
+      description: 'Neuf, jamais utilisé',
+      urlImg: '../assets/images/jouet.jpeg',
+      textAltImg: 'Un jouet pour enfant',
+      available: true,
+    },
+  ];
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -8,15 +8,20 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class ArticleComponent implements OnInit {
   //titreArticle: string = "Titre de l'article";
   //prixArticle: number = 12;
-  textAltImg: string = "Titre alternatif de l'image";
-  urlImg: string = 'https://via.placeholder.com/400x250';
+  //urlImg: string = 'https://via.placeholder.com/400x250';
+  //textAltImg: string = "Titre alternatif de l'image";
+  //available: boolean = false;
+
   totalNbrLike: number = 0;
   comment: string = 'Ceci est un commentaire';
-  available: boolean = false;
+  @Output() info = new EventEmitter<string>();
 
   @Input() titreArticle: string;
   @Input() prixArticle: number;
-  @Output() info = new EventEmitter<string>();
+  @Input() description: string;
+  @Input() urlImg: string;
+  @Input() textAltImg: string;
+  @Input() available: boolean;
 
   constructor() {}
 
