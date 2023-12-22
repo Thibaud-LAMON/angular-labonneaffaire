f64d865a2534ddd0a389fd4a33ce6b7dc6251daa
Thibaud-LAMON <thib.lamon@gmail.com>
2023-12-22 12:09:04 +0100

    Utiliser un service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  prix1: number = 80;
-  prix2: number = 230;
-  prix3: number = 15;
+export class AppComponent implements OnInit {
   message: string = '';
+  liste;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.liste = this.dataService.listeArticles;
+  }
 
   onAffiche(arg: string) {
     return (this.message = "Merci d'avoir voté pour l'article : " + arg);
   }
-
-  liste = [
-    {
-      titreArticle: 'Vélo',
-      prixArticle: 80,
-      description: 'Un super vélo tout terrain',
-      urlImg: '../assets/images/velo.jpeg',
-      textAltImg: 'Un vélo',
-      available: false,
-    },
-    {
-      titreArticle: 'TV',
-      prixArticle: 230,
-      description: "Très bonne qualité d'écran",
-      urlImg: '../assets/images/tv.jpeg',
-      textAltImg: 'Une télévision',
-      available: true,
-    },
-    {
-      titreArticle: 'Jouet',
-      prixArticle: 14,
-      description: 'Neuf, jamais utilisé',
-      urlImg: '../assets/images/jouet.jpeg',
-      textAltImg: 'Un jouet pour enfant',
-      available: true,
-    },
-  ];
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
-  constructor() { }
+  listeArticles = [
+    {
+      titreArticle: 'Vélo',
+      prixArticle: 80,
+      description: 'Un super vélo tout terrain',
+      urlImg: '../assets/images/velo.jpeg',
+      textAltImg: 'Un vélo',
+      available: false,
+    },
+    {
+      titreArticle: 'TV',
+      prixArticle: 230,
+      description: "Très bonne qualité d'écran",
+      urlImg: '../assets/images/tv.jpeg',
+      textAltImg: 'Une télévision',
+      available: true,
+    },
+    {
+      titreArticle: 'Jouet',
+      prixArticle: 14,
+      description: 'Neuf, jamais utilisé',
+      urlImg: '../assets/images/jouet.jpeg',
+      textAltImg: 'Un jouet pour enfant',
+      available: true,
+    },
+  ];
+  constructor() {}
 }
