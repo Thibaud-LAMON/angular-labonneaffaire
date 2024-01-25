e2cd2d18a8da11dd4ade79de7ab44b70b51847e1
Thibaud-LAMON <thib.lamon@gmail.com>
2024-01-25 17:26:03 +0100

    Utiliser les observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  secondes: number = 0;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const compteur = interval(1000); //initialisation d'intervale
+
+    //écoute d'intervale
+    compteur.subscribe({
+      next: (v) => (this.secondes = v), //propriété d'objet donc entre accolade
+      error: (e) => console.error(e),
+      complete: () => console.info('complete'),
+    });
+  }
 }
